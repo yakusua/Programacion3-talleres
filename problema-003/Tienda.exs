249149fd1a544c084a0bbbf66ccd7a3b81faa283
yakusua <lopezramirezjoancamilo@gmail.com>
2026-09-10 11:12:33 -0500

    ahora problema 3 tiene manejo de errores

--- problema-003/Tienda.exs
+++ problema-003/Tienda.exs
@@ -4,7 +4,7 @@
 defmodule Tienda do
   @moduledoc """
   Módulo que representa una tienda de productos.
-  -version 1.0
+  -versio 0.0.0.1
   -autor: Joan Camilo Lopez Ramirez
   -fecha: 2026-08-09
   """
@@ -19,15 +19,33 @@ defmodule Tienda do
     valor_entregado = "ingresar el valor de pago: "
     |> Utils.ingresar(:entero)
 
-    calcular_devuelta(valor_total, valor_entregado)
-    |> generar_mensaje()
-    |> IO.puts()
+    cond do
+      valor_total < 0 ->
+        IO.puts("no valores negativos")
+
+      valor_entregado < 0 ->
+        IO.puts("no valores negativos")
+
+      valor_total > valor_entregado ->
+        IO.puts("valor pagado insuficiente")
+
+      true ->
+        calcular_devuelta(valor_total, valor_entregado)
+        |> generar_mensaje()
+        |> IO.puts()
+    end
   end
 
+  @doc """
+  Función para calcular el valor de la devuelta.
+  """
   def calcular_devuelta(valor_total , valor_entregado) do
     valor_total - valor_entregado
   end
 
+  @doc """
+  Función para generar el mensaje de la devuelta
+  """
   def generar_mensaje(devuelta) do
     "la devuelta es #{devuelta}"
   end
--- problema-003/util.ex
+++ problema-003/util.ex
@@ -1,16 +1,32 @@
 defmodule Utils do
 
-  #pattern matching
   def ingresar(mensaje, :texto) do
-    mensaje
-    |> IO.gets()
-    |> String.trim()
+    resultado = IO.gets(mensaje)
+    if resultado == "" do
+      throw("Error: No se puede ingresar un valor vacío")
+    end
+    String.trim(resultado)
   end
 
   def ingresar(mensaje, :entero) do
-    mensaje
-    |> IO.gets()
-    |> String.trim()
-    |> String.to_integer()
+    try do
+      resultado = IO.gets(mensaje)
+      texto = String.trim(resultado)
+
+      if texto == "" do
+        throw("Error: No se puede ingresar un valor vacío")
+      end
+
+      String.to_integer(texto)
+    rescue
+      ArgumentError ->
+        IO.puts("Error: Debes ingresar un número válido")
+        :error
+    catch
+      _, mensaje_error ->
+        IO.puts(mensaje_error)
+        :error
+    end
   end
+
 end
